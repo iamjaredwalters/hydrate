a63f1588226cd1bed2996f4db360b2d4c71a8318
Jared Walters <jared.w.walters@gmail.com>
2016-12-23 22:16:17 -0800

    Coordinate parent state when child component is updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,63 @@ import './App.css';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      registeredCounters: [],
+      needWater: 'No'
+    };
+
+    this.status = this.status.bind(this);
+    this.registerCounter = this.registerCounter.bind(this);
+    this.updateCounter = this.updateCounter.bind(this);
+  }
+
+  status(counter) {
+    this.updateCounter(counter);
+
+    // If you've had more alcohol than water then you need water
+    this.setState((prevState) => {
+      const isDrunk = prevState.registeredCounters[0].count > prevState.registeredCounters[1].count;
+      const getWater = isDrunk ? 'Yes' : 'No';
+      return({
+        needWater: getWater
+      })
+    });
+  }
+
+  updateCounter(counter) {
+    this.setState((prevState) => {
+      let newState = [];
+
+      prevState.registeredCounters.forEach((registeredCounter) => {
+        // Increment the count of the counter that was updated
+        if(counter.name === registeredCounter.name) {
+          registeredCounter.count++;
+        }
+        newState.push(registeredCounter);
+      });
+
+      return({
+        registeredCounters: newState
+      })
+    });
+  }
+
+  registerCounter(counter) {
+    // Register any counters that are child components
+    this.setState((prevState) => {
+      // Use empty array if no counters exist
+      const counters = prevState.registeredCounters.length > 0 ? prevState.registeredCounters : [];
+
+      counters.push(counter);
+
+      return({
+        registeredCounters: counters
+      })
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,8 +69,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Hydrate</h2>
         </div>
-        <Counter increment={1} name="Alcohol" />
-        <Counter increment={1} name="Water" />
+        <h3>Do you need water? {this.state.needWater}</h3>
+        <Counter increment={1} name="Alcohol" onClick={this.status} registerCounter={this.registerCounter} />
+        <Counter increment={1} name="Water" onClick={this.status} registerCounter={this.registerCounter} />
       </div>
     );
   }
--- src/Counter.js
+++ src/Counter.js
@@ -7,6 +7,28 @@ class Counter extends Component {
     this.state = {counter: 0};
 
     this.increment = this.increment.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  // Register a counter only when its mounted
+  componentDidMount() {
+    const counter = {
+      name: this.props.name,
+      count: this.state.counter
+    };
+
+    this.props.registerCounter(counter);
+  }
+
+  onClick() {
+    this.increment();
+
+    const counter = {
+      name: this.props.name,
+      count: this.state.counter
+    };
+
+    this.props.onClick(counter); // Passed down from parent
   }
 
   increment() {
@@ -19,7 +41,7 @@ class Counter extends Component {
 
   render() {
     return (
-      <div className="counter" onClick={this.increment}>
+      <div className="counter" onClick={this.onClick}>
         <div className="counter-container">
           <div className="title">{this.props.name}</div>
           <p className="count">{this.state.counter}</p>
